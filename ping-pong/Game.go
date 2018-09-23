46dcd356e92b094f4c8ef7ce963cc891f835bb7f
EvilKhaosKat <evilkhaoskat@gmail.com>
2018-09-23 14:57:24 +0300

    [ping-ping] ball coordinate update

--- ping-pong/Game.go
+++ ping-pong/Game.go
@@ -7,15 +7,15 @@ const (
 )
 
 type Game struct {
-	table                   Table
-	leftPlayer, rightPlayer Player
+	table                   *Table
+	leftPlayer, rightPlayer *Player
 }
 
 //Table describes table state
 type Table struct {
 	width, height     int
-	leftBat, rightBat Bat
-	ball              Ball
+	leftBat, rightBat *Bat
+	ball              *Ball
 }
 
 type Bat struct {
@@ -29,7 +29,7 @@ type Ball struct {
 
 type Player struct {
 	name  string
-	bat   Bat
+	bat   *Bat
 	score int
 }
 
@@ -45,21 +45,62 @@ func NewGame() Game {
 	}
 }
 
-func newTable(leftBat, rightBat Bat) Table {
-	return Table{TableWidth, TableHeight,
+func (game *Game) tick() {
+	game.updateBallCoor()
+}
+
+func (game *Game) updateBallCoor() {
+	table := game.table
+
+	ball := table.ball
+
+	height := table.height
+	width := table.width
+
+	updateBallX(ball, width)
+	updateBallY(ball, height)
+
+}
+
+func updateBallX(ball *Ball, width int) {
+	ball.x = ball.x + ball.xSpeed
+	if ball.x > width {
+		ball.x = width - (ball.x - width)
+		ball.xSpeed = -ball.xSpeed
+	}
+	if ball.x < 0 {
+		ball.x = -ball.x
+		ball.xSpeed = -ball.xSpeed
+	}
+}
+
+func updateBallY(ball *Ball, height int) {
+	ball.y = ball.y + ball.ySpeed
+	if ball.y > height {
+		ball.y = height - (ball.y - height)
+		ball.ySpeed = -ball.ySpeed
+	}
+	if ball.y < 0 {
+		ball.y = -ball.y
+		ball.xSpeed = -ball.xSpeed
+	}
+}
+
+func newTable(leftBat, rightBat *Bat) *Table {
+	return &Table{TableWidth, TableHeight,
 		leftBat,
 		rightBat,
 		newBall()}
 }
 
-func newBat() Bat {
-	return Bat{TableHeight/2 - BatLength/2, BatLength}
+func newBat() *Bat {
+	return &Bat{TableHeight/2 - BatLength/2, BatLength}
 }
 
-func newPlayer(name string, bat Bat) Player {
-	return Player{name, bat, 0}
+func newPlayer(name string, bat *Bat) *Player {
+	return &Player{name, bat, 0}
 }
 
-func newBall() Ball {
-	return Ball{TableWidth / 2, TableHeight / 2, 0, 0}
+func newBall() *Ball {
+	return &Ball{TableWidth / 2, TableHeight / 2, 0, 0}
 }
