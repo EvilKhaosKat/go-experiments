06d90c378beaf58e25ae1d126d614f006390e5d0
EvilKhaosKat <evilkhaoskat@gmail.com>
2018-10-21 23:55:55 +0300

    [ping-pong] build scripts updated

--- ping-pong/build_mac.sh
+++ ping-pong/build_mac.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-env GOOS=darwin GOARCH=amd64 go build -o ping_pong main.go Game.go Server.go Client.go Ui.go
+env GOOS=darwin GOARCH=amd64 go build -o ping_pong_mac main.go Game.go Server.go Client.go Ui.go
